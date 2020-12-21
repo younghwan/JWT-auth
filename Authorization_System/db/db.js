617a41a158b8288d720b2efc3806338847d722d4
younghwan <show2881@gmail.com>
2020-12-21 12:19:38 +0900

    indent

--- Authorization_System/db/db.js
+++ Authorization_System/db/db.js
@@ -1,8 +1,7 @@
 const mysql = require('mysql2');
 const dbconfig = require('../config/database.js')
-
 const connection = mysql.createConnection(dbconfig)
 const connectionPromise = connection.promise()
-// module.exports = connection;
+
 module.exports = connectionPromise;
 
